420fd1d0531cd363c1fff06b1a4c68c6074bbf34
ivan.mamai00@mail.ru <250219As>
2021-03-26 11:11:46 +0300

    2

--- CoffeBoxClass.h
+++ CoffeBoxClass.h
@@ -1,40 +1,5 @@
 #pragma once
-#include <iostream>
-#include <Windows.h>
-#include <conio.h>
-
-using namespace std;
-
 class CoffeBoxClass
 {
-private:
-	const int PIN = 1111;
-	const int ESPRESSO_PRICE = 2;
-	const int CAPPUCCINO_PRICE = 3;
-	const double LATTE_PRICE = 2.5;
-	double moneyClient = 0;
-	double moneyCoffeeBox = 0;
-	int numberCups = 7;
-	bool isPinTrue = true;
-
-	int attempts = 1;//Vlad
-
-	void printMenu();
-	double enterData();
-	void selectingAnAction(double number);
-	void depositMoney();
-	void buyCoffee(double priceCoffee, string typeCoffee);
-	//vlad
-	void managingTheService();
-	bool isTruePassword(double number);
-	void verificationServise();//I
-	void serviseMenu();
-	void balance();
-	void cupsEmpty();
-
-public:
-	void start();
 };
-
-
 
